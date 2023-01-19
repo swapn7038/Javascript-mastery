a1e433ad854b21c5258a05680436ed6e9ed33770
Swapnil Shelke <Swapnilbabanshelke@gmail.com>
2023-01-19 16:55:51 +0530

    Completed...

---
+++ JsThapa/script.js
@@ -0,0 +1,7 @@
+const name = "swapnil";
+const age = 25;
+
+// console.log(name);
+
+console.log(typeof name);
+console.log(typeof age);
--- js+king/English Dictionary/assets/script.js
+++ js+king/English Dictionary/assets/script.js
@@ -16,6 +16,21 @@ async function fetchAPI(word) {
     `;
     const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
     const result = await fetch(url).then((res) => res.json());
+
+    if (result.title) {
+      meaningContainerEl.style.display = "block";
+      infoTextEl.style.display = "none";
+      titleEl.innerText = word;
+      meaningEl.innerText = "Not available";
+      audioEl.style.display = "none";
+    } else {
+      infoTextEl.style.display = "none";
+      meaningContainerEl.style.display = "block";
+      audioEl.style.display = "inline-flex";
+      titleEl.innerText = result[0].word;
+      meaningEl.innerText = result[0].meanings[0].definitions[0].definition;
+      audioEl.src = result[0].phonetics[0].audio;
+    }
     //   console.log(result);
 
     infoTextEl.style.display = "none";
@@ -25,6 +40,9 @@ async function fetchAPI(word) {
     audioEl.src = result[0].phonetics[0].audio;
   } catch (error) {
     console.log(error);
+    infoTextEl.innerText = `
+    an error occured, try again!!!
+  `;
   }
 }
 
@@ -36,3 +54,5 @@ inputEl.addEventListener("keyup", (e) => {
 });
 
 // 'await' expressions are only allowed within async functions and at the top levels of modules.ts(1308)
+
+// 3:15:00
