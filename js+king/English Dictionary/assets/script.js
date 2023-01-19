8268f981a4c15ac795fa4ad8c0e3713a4c71423d
Swapnil Shelke <Swapnilbabanshelke@gmail.com>
2023-01-19 12:25:09 +0530

    working...

--- js+king/English Dictionary/assets/script.js
+++ js+king/English Dictionary/assets/script.js
@@ -3,6 +3,7 @@ const infoTextEl = document.getElementById("info-text");
 const meaningContainerEl = document.getElementById("meaning-container");
 const titleEl = document.getElementById("title");
 const meaningEl = document.getElementById("meaning");
+const audioEl = document.getElementById("audio");
 
 async function fetchAPI(word) {
   try {
@@ -20,8 +21,8 @@ async function fetchAPI(word) {
     infoTextEl.style.display = "none";
     meaningContainerEl.style.display = "block";
     titleEl.innerText = result[0].word;
-    meaningEl.innerText = result[0].meanings[0];
-    definitions[0];
+    meaningEl.innerText = result[0].meanings[0].definitions[0].definition;
+    audioEl.src = result[0].phonetics[0].audio;
   } catch (error) {
     console.log(error);
   }
