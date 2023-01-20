9d95fe3b262c873d615a5be1d391d07b7bf1c084
Swapnil Shelke <Swapnilbabanshelke@gmail.com>
2023-01-20 17:16:25 +0530

    Completed

--- js+king/anime+generator/assets/script.js
+++ js+king/anime+generator/assets/script.js
@@ -3,7 +3,24 @@ const animeContainerEl = document.querySelector(".anime-container");
 const animeImgEl = document.querySelector(".anime-img");
 const animeNameEl = document.querySelector(".anime-name");
 
-btnEl.addEventListener("click", () => {
+btnEl.addEventListener("click", async function () {
   try {
-  } catch (error) {}
+    btnEl.disabled = true;
+    btnEl.innerText = "Loadin...";
+    animeNameEl.innerText = "Updating...";
+    animeImgEl.src = "loader.svg";
+    const response = await fetch("https://api.catboys.com/img");
+    const data = await response.json();
+    btnEl.disabled = false;
+    btnEl.innerText = "Get Anime";
+    animeContainerEl.style.display = "block";
+    animeImgEl.src = data.url;
+    animeNameEl.innerText = data.artist;
+    // console.log(data);
+  } catch (error) {
+    console.log(error);
+    btnEl.disabled = false;
+    btnEl.innerText = "Get Anime";
+    animeNameEl.innerText = "Error happened";
+  }
 });
