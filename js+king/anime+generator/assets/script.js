b40d35a58061c1d11ee9d2060f13f228457e70f7
Swapnil Shelke <Swapnilbabanshelke@gmail.com>
2023-01-20 16:52:06 +0530

    Updated

---
+++ js+king/anime+generator/assets/script.js
@@ -0,0 +1,9 @@
+const btnEl = document.getElementById("btn");
+const animeContainerEl = document.querySelector(".anime-container");
+const animeImgEl = document.querySelector(".anime-img");
+const animeNameEl = document.querySelector(".anime-name");
+
+btnEl.addEventListener("click", () => {
+  try {
+  } catch (error) {}
+});
---
+++ js+king/readme.md
@@ -0,0 +1,17 @@
+# JS King projects
+
+Projects completed
+
+1. Button ripple
+2. Counter
+3. Dad jokes generator
+4. Ripple
+5. Quote generator
+6. Feedback UI
+7. English Dictionary
+8.
+
+Project not working
+
+1. Rotatig gallery
+2. New year countdown
